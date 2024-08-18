6773237d7d2b169620c923f663e9e8c1ce870eaf
dmytrovoytko <dmytro.voytko@gmail.com>
2024-08-18 11:03:01 +0300

    Better formatting

--- train_model/predict.py
+++ train_model/predict.py
@@ -5,16 +5,18 @@ from sklearn.metrics import (
     accuracy_score,
     balanced_accuracy_score,
     classification_report,
-    confusion_matrix
+    confusion_matrix,
 )
 
 from preprocess import enc_load, preprocess_data, model_dir
 from settings import DATA_DIR, DATASET_NUM, MODEL_DIR, TARGET
 
-from settings import DEBUG # isort:skip
+from settings import DEBUG  # isort:skip
+
 # DEBUG = True # True # False # override global settings
 
-from utils import S3_ENDPOINT_URL, S3_BUCKET # isort:skip
+from utils import S3_ENDPOINT_URL, S3_BUCKET  # isort:skip
+
 
 def print_results(classifier, y_test, predict_y, verbose=DEBUG):
     print(f'\n================\n{classifier}')
@@ -23,6 +25,7 @@ def print_results(classifier, y_test, predict_y, verbose=DEBUG):
         print(confusion_matrix(y_test, predict_y))
     print(f' accuracy: {accuracy_score(y_test, predict_y):05.3f}')
     print(f' balanced accuracy: {balanced_accuracy_score(y_test, predict_y):05.3f}')
+
 
 def predict_df(df, MODEL_DIR, verbose=DEBUG):
     print(f'\nPredicting using model {MODEL_DIR}')
@@ -58,15 +61,18 @@ def predict_df(df, MODEL_DIR, verbose=DEBUG):
         print(f'!!! Exception while predicting {TARGET}:', e)
         return pd.Series()
 
+
 def predict_dict(object, dataset_num, verbose=False):
     # transform dict to create df from it
-    dct = {k:[v] for k,v in object.items()}
+    dct = {k: [v] for k, v in object.items()}
     df = pd.DataFrame(dct)
     ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
     df = preprocess_data(df, ord_enc, dataset_num, fit_enc=False)
     # model_dir = f'./model/{dataset_num}/'
     y_pred = predict_df(df, model_dir(dataset_num), verbose=verbose)
-    result = { str(TARGET).lower(): int(list(y_pred)[0]) } # explicit int() is reqiered for serialization
+    result = {
+        str(TARGET).lower(): int(list(y_pred)[0])
+    }  # explicit int() is reqiered for serialization
     if DEBUG:
         print('result:', result)
 
@@ -74,79 +80,21 @@ def predict_dict(object, dataset_num, verbose=False):
 
 
 if __name__ == '__main__':
-    if DATASET_NUM==1:
-        # dataset 1
-        data = [
-        (50001,1,4.0,'Mobile Phone',3,6.0,'Debit Card','Female',3.0,3,'Laptop & Accessory',2,'Single',9,1,11.0,1.0,1.0,5.0,159.93),
-        (50002,1,None,'Phone',1,8.0,'UPI','Male',3.0,4,'Mobile',3,'Single',7,1,15.0,0.0,1.0,0.0,120.9),
-        (50003,1,None,'Phone',1,30.0,'Debit Card','Male',2.0,4,'Mobile',3,'Single',6,1,14.0,0.0,1.0,3.0,120.28),
-        (50004,1,0.0,'Phone',3,15.0,'Debit Card','Male',2.0,4,'Laptop & Accessory',5,'Single',8,0,23.0,0.0,1.0,3.0,134.07),
-        (50005,1,0.0,'Phone',1,12.0,'CC','Male',None,3,'Mobile',5,'Single',3,0,11.0,1.0,1.0,3.0,129.6),
-        (50006,1,0.0,'Computer',1,22.0,'Debit Card','Female',3.0,5,'Mobile Phone',5,'Single',2,1,22.0,4.0,6.0,7.0,139.19),
-        (50030,0,5.0,'Computer',3,14.0,'E wallet','Female',2.0,3,'Fashion',2,'Single',2,0,14.0,2.0,3.0,7.0,189.98),
-        (50031,0,2.0,'Computer',1,6.0,'COD','Male',2.0,3,'Laptop & Accessory',3,'Divorced',2,0,13.0,0.0,1.0,9.0,143.19),
-        ]
+    # quick tests
+    print('\nTesting predict_df...')
+    from preprocess import load_data
 
-        columns = [
-        'CustomerID','Churn','Tenure','PreferredLoginDevice','CityTier','WarehouseToHome','PreferredPaymentMode','Gender',
-        'HourSpendOnApp','NumberOfDeviceRegistered','PreferedOrderCat','SatisfactionScore','MaritalStatus',
-        'NumberOfAddress','Complain','OrderAmountHikeFromlastYear','CouponUsed','OrderCount',
-        'DaySinceLastOrder','CashbackAmount'
-        ]
-        df = pd.DataFrame(data, columns=columns)
-        # df.drop(['CustomerID', 'Churn'], axis=1, inplace = True)
-        dataset_num = 1
-        ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
-        df = preprocess_data(df, ord_enc, dataset_num, fit_enc=False)
-        # print('OrdinalEncoder categories:', ord_enc.categories_)
-        # # df = preprocess_data(df, ord_enc, 1, fit_enc=True)
-        # import numpy as np
-        # categorical_features = df.select_dtypes(exclude=[np.number]).columns
-        # print(categorical_features)
-        # df[categorical_features] = ord_enc.fit_transform(df[categorical_features])
-
-        print(df.to_string())
-        # exit()
-    elif DATASET_NUM==2:
-        # dataset 2
-        data = [
-        (202006,45759018157,'Female','No','Yes',1,'No','No','Yes','Yes',29.85,29.85,'No'),
-        (202006,45315483266,'Male','No','Yes',60,'Yes','No','No','Yes',20.5,1198.8,'No'),
-        (202006,45236961615,'Male','No','No',5,'Yes','Yes','Yes','No',104.1,541.9,'Yes'),
-        (202006,45929827382,'Female','No','Yes',72,'Yes','Yes','Yes','Yes',115.5,8312.75,'No'),
-        (202006,45305082233,'Female','No','Yes',56,'Yes','Yes','Yes','No',81.25,4620.4,'No'),
-        ]
-
-        columns = [
-        'UpdatedAt','customerID','gender','SeniorCitizen','Partner','tenure','PhoneService','StreamingTV',
-        'InternetService','PaperlessBilling','MonthlyCharges','TotalCharges','Churn',
-        ]
-        df = pd.DataFrame(data, columns=columns)
-        # df.drop(['CustomerID', 'Churn'], axis=1, inplace = True)
-        dataset_num = 2
-        ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
-        df = preprocess_data(df, ord_enc, dataset_num, fit_enc=False)
-        # print('OrdinalEncoder categories:', ord_enc.categories_)
-        # # df = preprocess_data(df, ord_enc, 1, fit_enc=True)
-        # import numpy as np
-        # categorical_features = df.select_dtypes(exclude=[np.number]).columns
-        # print(categorical_features)
-        # df[categorical_features] = ord_enc.fit_transform(df[categorical_features])
-
-        print(df.to_string())
-        # exit()
-    else:
-        print('!!! Predict test, incorrect dataset number:', DATASET_NUM)
-        exit(1)
-
+    df = load_data(DATASET_NUM).head(10)
     y_pred = predict_df(df, MODEL_DIR, verbose=True)
 
     columns = df.columns.to_list()
     if TARGET in columns:
         print_results('Saved model', df[TARGET], y_pred, verbose=True)
 
+    print('\nTesting predict_dict...')
     # test predict_dict .drop([TARGET], axis=1)
-    row = df.to_dict('records')[0]
+    row = df.head(1).to_dict('records')[0]
+    print(row)
     expected_result = row.pop(TARGET, None)
     result = predict_dict(row, DATASET_NUM, verbose=True)
     print(f'\npredict_dict: data {row} -> {result} // expected: {expected_result}')
--- train_model/preprocess.py
+++ train_model/preprocess.py
@@ -14,25 +14,27 @@ from settings import DATA_DIR, DATASET_NUM, MODEL_DIR, REMOVE_OUTLIERS, TARGET
 # pd.set_option('display.max_rows', None)
 
 
+from settings import DEBUG  # isort:skip
 
-from settings import DEBUG # isort:skip
-DEBUG = True # True # False # override global settings
+DEBUG = True  # True # False # override global settings
 
-from utils import S3_ENDPOINT_URL, S3_BUCKET # isort:skip
+from utils import S3_ENDPOINT_URL, S3_BUCKET  # isort:skip
+
 
 def model_dir(dataset_num=DATASET_NUM):
     return f'./model/{dataset_num}/'  # ! with '/' at the end!
 
+
 def load_dataset(dataset_num=DATASET_NUM):
-    print('\nLoading dataset...')
-    if dataset_num==1:
+    print(f'\nLoading dataset [{dataset_num}]...')
+    if dataset_num == 1:
         file_name = 'E Commerce Dataset.xlsx'
         data = pd.read_excel(DATA_DIR + file_name, sheet_name='E Comm')
         if DEBUG:
             data_dict = pd.read_excel(DATA_DIR + 'E Commerce Dataset.xlsx', sheet_name='Data Dict')
-            data.to_csv(DATA_DIR+file_name+'.csv', encoding='utf-8', index=False)
-            data_dict.to_csv(DATA_DIR+file_name[:-5]+'_dict.csv', encoding='utf-8', index=False)
-    elif dataset_num==2:
+            data.to_csv(DATA_DIR + file_name + '.csv', encoding='utf-8', index=False)
+            data_dict.to_csv(DATA_DIR + file_name[:-5] + '_dict.csv', encoding='utf-8', index=False)
+    elif dataset_num == 2:
         file_name = 'dqlab_telco_final.csv'
         data = pd.read_csv(DATA_DIR + file_name)
     else:
@@ -41,11 +43,8 @@ def load_dataset(dataset_num=DATASET_NUM):
 
     if DEBUG:
         print(f' Loaded {data.shape[0]} records.')
-        # print(data.dtypes.to_string())
-        print(pd.DataFrame(data.info()).to_string())
-        # print(data_dict.dtypes)
+        data.info()
         print(data.head())
-        # print(data_dict.head())
 
     return data
 
@@ -59,6 +58,7 @@ def determine_outlier_thresholds_iqr(df, col_name, th1=0.25, th3=0.75):
     lower_limit = quartile1 - 1.5 * iqr
     return lower_limit, upper_limit
 
+
 def determine_outlier_thresholds_sdm(df, col_name, scale):
     # for removing outliers using the Standard deviation method
     df_mean = df[col_name].mean()
@@ -67,6 +67,7 @@ def determine_outlier_thresholds_sdm(df, col_name, scale):
     lower_limit = df_mean - scale * df_std
     return lower_limit, upper_limit
 
+
 def print_missing_values_table(data, na_name=False):
     na_columns = [col for col in data.columns if data[col].isnull().sum() > 0]
     n_miss = data[na_columns].isnull().sum().sort_values(ascending=False)
@@ -74,7 +75,7 @@ def print_missing_values_table(data, na_name=False):
     missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
     print(missing_df, end="\n")
     if na_name:
-            return na_columns
+        return na_columns
 
 
 def preprocess_df(data):
@@ -85,24 +86,28 @@ def preprocess_df(data):
     columns = data.columns.to_list()
 
     # 1. drop useless columns
-    useless_columns = ['CustomerID', 'customerID', 'UpdatedAt', ] #'InternetService']
+    useless_columns = [
+        'CustomerID',
+        'customerID',
+        'UpdatedAt',
+    ]
     for col in useless_columns:
-        if col in columns: # exists in df
-            if DEBUG and col.lower()=='customerid':
+        if col in columns:  # exists in df
+            if DEBUG and col.lower() == 'customerid':
                 print(f'Total rows: {total_rows_number}, unique {col}s: {data[col].nunique()}')
-            data.drop([col], axis=1, inplace = True)
+            data.drop([col], axis=1, inplace=True)
 
     # 2. inspect categorical columns
-    categorical = data.dtypes[data.dtypes=='object'].keys()
+    categorical = data.dtypes[data.dtypes == 'object'].keys()
     if target in categorical:
-        # specifics of DATASET_NUM==2, TARGET must be encoded to 0/1 before using OrdinalEncoder
-        data.loc[data[target] ==  'No', target]=0
-        data.loc[data[target] == 'Yes', target]=1
+        # specifics of DATASET_NUM==2, TARGET must be encoded to 0/1 BEFORE using OrdinalEncoder
+        data.loc[data[target] == 'No', target] = 0
+        data.loc[data[target] == 'Yes', target] = 1
         data[target] = data[target].astype(int)
         # update categorical
-        categorical = data.dtypes[data.dtypes=='object'].keys()
+        categorical = data.dtypes[data.dtypes == 'object'].keys()
         if DEBUG:
-            print(target,'encoded')
+            print(target, 'encoded')
 
     if DEBUG and target in columns:
         print(f'\nCategorical columns: {list(categorical)}')
@@ -110,21 +115,24 @@ def preprocess_df(data):
         for col in categorical:
             print('\n by', data[col].value_counts().to_string())
 
-        if len(data)>10:
+        if len(data) > 10:
             corr = data.corr(numeric_only=True)[target]
             print(f'\nCorrelation to {target}:\n{corr.to_string()}')
 
     # 3. inspect missing values
     nan_cols = data.columns[data.isnull().any()].to_list()
-    if DEBUG:
-            #list of columns with missing values and its percentage
-            print(f'\nColumns with nulls:\n{nan_cols}')
-            print_missing_values_table(data, na_name=True)
+    if DEBUG and nan_cols:
+        # list of columns with missing values and its percentage
+        print(f'\nColumns with nulls:\n{nan_cols}')
+        print_missing_values_table(data, na_name=True)
 
     # 4. fix missing values - fill with median values
     # fix_cols = ["Tenure","DaySinceLastOrder","OrderAmountHikeFromlastYear","OrderCount",
     #             "CouponUsed","HourSpendOnApp","WarehouseToHome",]
-    data.loc[:,nan_cols] = data.loc[:,nan_cols].fillna(data.loc[:,nan_cols].median())
+    if nan_cols:
+        if DEBUG:
+            print(f'\nFixing missing values...')
+        data.loc[:, nan_cols] = data.loc[:, nan_cols].fillna(data.loc[:, nan_cols].median())
 
     if DEBUG:
         nan_cols = data.columns[data.isnull().any()].to_list()
@@ -144,14 +152,17 @@ def preprocess_df(data):
                 print(" lower limit:", lower)
                 data = data[(data[col] >= lower) & (data[col] <= upper)]
 
-    print(f'\nFinal number of records:', data.shape[0],'/',total_rows_number,
-                            '=', f'{data.shape[0]/total_rows_number*100:05.2f}%','\n')
+    print(
+        f'\nFinal number of records: {data.shape[0]} / {total_rows_number} =',
+        f'{data.shape[0]/total_rows_number*100:05.2f}%\n',
+    )
     return data
 
 
 def enc_save(enc, file_name):
     with open(file_name, 'wb') as f:
         pickle.dump(enc, f)
+
 
 def enc_load(file_name):
     # if S3_ENDPOINT_URL:
@@ -164,6 +175,7 @@ def enc_load(file_name):
         enc = pickle.load(f)
         return enc
     return OrdinalEncoder()
+
 
 def preprocess_data(df, ord_enc, dataset_num=DATASET_NUM, fit_enc=False):
     # fix missing values, remove outliers
@@ -187,7 +199,7 @@ def preprocess_data(df, ord_enc, dataset_num=DATASET_NUM, fit_enc=False):
             df[categorical_features] = ord_enc.transform(df[categorical_features])
 
     columns = df.columns.to_list()
-    if DEBUG and TARGET in columns and len(df)>10:
+    if DEBUG and TARGET in columns and len(df) > 10:
         corr = df.corr(numeric_only=True)[TARGET]
         print(f'\nCorrelation-2 to {TARGET}:\n{corr.to_string()}')
 
@@ -195,11 +207,11 @@ def preprocess_data(df, ord_enc, dataset_num=DATASET_NUM, fit_enc=False):
 
 
 def load_data(dataset_num=DATASET_NUM):
-    if dataset_num==1:
+    if dataset_num == 1:
         df = load_dataset(1)
         ord_enc = OrdinalEncoder()
         df = preprocess_data(df, ord_enc, 1, fit_enc=True)
-    elif dataset_num==2:
+    elif dataset_num == 2:
         df = load_dataset(2)
         ord_enc = OrdinalEncoder()
         df = preprocess_data(df, ord_enc, 2, fit_enc=True)
@@ -209,58 +221,19 @@ def load_data(dataset_num=DATASET_NUM):
 
     return df
 
+
 if __name__ == '__main__':
-    # full dataset 1
-    df = load_data(1)
-    print('\nreal 1\n', df.head(5).to_string())
-    # full dataset 2
-    df = load_data(2)
-    print('\nreal 2\n', df.head(5).to_string())
-    # exit()
+    # quick tests
+    for dataset_num in [1, 2]:
+        # full dataset
+        df = load_data(dataset_num)  # load dataset and train encoder
+        print(f'\nfull dataset {dataset_num} head(5)\n', df.head(5).to_string())
 
-    # mini dataset 1
-    data = [
-        (50001,1,4.0,'Mobile Phone',3,6.0,'Debit Card','Female',3.0,3,'Laptop & Accessory',2,'Single',9,1,11.0,1.0,1.0,5.0,159.93),
-        (50002,1,None,'Phone',1,8.0,'UPI','Male',3.0,4,'Mobile',3,'Single',7,1,15.0,0.0,1.0,0.0,120.9),
-        (50003,1,None,'Phone',1,30.0,'Debit Card','Male',2.0,4,'Mobile',3,'Single',6,1,14.0,0.0,1.0,3.0,120.28),
-        (50004,1,0.0,'Phone',3,15.0,'Debit Card','Male',2.0,4,'Laptop & Accessory',5,'Single',8,0,23.0,0.0,1.0,3.0,134.07),
-        (50005,1,0.0,'Phone',1,12.0,'CC','Male',None,3,'Mobile',5,'Single',3,0,11.0,1.0,1.0,3.0,129.6),
-        (50006,1,0.0,'Computer',1,22.0,'Debit Card','Female',3.0,5,'Mobile Phone',5,'Single',2,1,22.0,4.0,6.0,7.0,139.19),
-    ]
-
-    columns = [
-        'CustomerID','Churn','Tenure','PreferredLoginDevice','CityTier','WarehouseToHome','PreferredPaymentMode','Gender',
-        'HourSpendOnApp','NumberOfDeviceRegistered','PreferedOrderCat','SatisfactionScore','MaritalStatus',
-        'NumberOfAddress','Complain','OrderAmountHikeFromlastYear','CouponUsed','OrderCount',
-        'DaySinceLastOrder','CashbackAmount'
-    ]
-    df = pd.DataFrame(data, columns=columns)
-    df1 = preprocess_df(df)
-    print('\n',1111,'\n', df1.head(1).to_string())
-    dataset_num = 1
-    ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
-    df = preprocess_data(df1, ord_enc, 1, fit_enc=False)
-    print('\n',1111-2,'\n', df.head(1).to_string())
-
-    # exit()
-
-    # dataset 2
-    data = [
-        (202006,45759018157,'Female','No','Yes',1,'No','No','Yes','Yes',29.85,29.85,'No'),
-        (202006,45315483266,'Male','No','Yes',60,'Yes','No','No','Yes',20.5,1198.8,'No'),
-        (202006,45236961615,'Male','No','No',5,'Yes','Yes','Yes','No',104.1,541.9,'Yes'),
-        (202006,45929827382,'Female','No','Yes',72,'Yes','Yes','Yes','Yes',115.5,8312.75,'No'),
-        (202006,45305082233,'Female','No','Yes',56,'Yes','Yes','Yes','No',81.25,4620.4,'No'),
-    ]
-
-    columns = [
-        'UpdatedAt','customerID','gender','SeniorCitizen','Partner','tenure','PhoneService','StreamingTV',
-        'InternetService','PaperlessBilling','MonthlyCharges','TotalCharges','Churn',
-    ]
-    df = pd.DataFrame(data, columns=columns)
-    df = preprocess_df(df)
-    print('\n',2222,'\n', df.head(1).to_string())
-    dataset_num = 2
-    ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
-    df = preprocess_data(df1, ord_enc, 1, fit_enc=False)
-    print('\n',2222-2,'\n', df.head(1).to_string())
+        # mini dataset 1
+        df = load_dataset(dataset_num).head(10)
+        # df1 = preprocess_df(df)
+        print(f'\nmini dataset {dataset_num}\n', df.head(1).to_string())
+        # loading trained encoder
+        ord_enc = enc_load(f'{model_dir(dataset_num)}encoder.pkl')
+        df = preprocess_data(df, ord_enc, 1, fit_enc=False)
+        print(f'\nmini encoded {dataset_num}\n', df.head(1).to_string())
